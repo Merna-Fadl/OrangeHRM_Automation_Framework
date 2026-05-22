7d9b0638d584c6ba7c6294891b7009f207ce9678
Merna-Fadl <137529881+Merna-Fadl@users.noreply.github.com>
2026-05-22 20:00:43 +0300

    Update PIMTest.java

--- src/test/java/tests/PIMTest.java
+++ src/test/java/tests/PIMTest.java
@@ -13,7 +13,6 @@ public class PIMTest extends BaseTest {
 
     @DataProvider(name = "employeeData")
     public Object[][] getEmployeeData() {
-        // بنحدد مسار ملف الـ JSON اللي فيه بيانات الموظفين
         return JsonReader.getJsonData("src/test/resources/testData.json");
     }
 
@@ -22,25 +21,23 @@ public class PIMTest extends BaseTest {
     @Test(dataProvider = "employeeData", description = "Add New Employee Flow")
     public void testAddNewEmployee(String fName, String mName, String lName, String id, String user, String pass) {
 
-        // 1. تسجيل الدخول (باستخدام الـ Fluent Design)
         loginPage.enterUsername(ConfigReader.getProperty("username"))
                 .enterPassword(ConfigReader.getProperty("password"))
                 .clickLogin()
                 .assertSuccessfulLogin();
 
-        // 2. التعامل مع صفحة الـ PIM
         PIMPage pimPage = new PIMPage(driver);
 
-        pimPage.navigateToPIM()            // الضغط على PIM في القائمة الجانبية
-                .navigateToAddEmployee()     // الضغط على Add Employee من فوق
+        pimPage.navigateToPIM()            
+                .navigateToAddEmployee()     
                 .fillFullName(fName, mName, lName)
                 .fillEmployeeId(id)
                 .setLoginCredentials(user, pass)
                 .clickSave()
-                .validateEmployeeSaved()   // التأكد من ظهور رسالة النجاح
-                // 3. Search & Verify Flow
-                .searchForAddedEmployee()     // يرجع للـ Employee List ويبحث بالـ ID
-                .validateEmployeeInTable()  // يتأكد إن الـ ID ظاهر في أول صف في الجدول
+                .validateEmployeeSaved()
+                //  Search & Verify Flow
+                .searchForAddedEmployee()   
+                .validateEmployeeInTable()  
                 .deleteSelectedEmployee(id)
                 .validateDeletionSuccess();
     }
