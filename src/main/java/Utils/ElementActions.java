c73964533e0a5b53e5cfbe33c9568ef4f0f0ee09
Merna-Fadl <137529881+Merna-Fadl@users.noreply.github.com>
2026-05-22 19:41:28 +0300

    Update ElementActions.java

--- src/main/java/Utils/ElementActions.java
+++ src/main/java/Utils/ElementActions.java
@@ -16,14 +16,12 @@ public class ElementActions {
     private GUIDriver driver;
     private  Scrolling scrolling;
     private Waits waits;
-    // تعريف الـ Logger
     private static final Logger logger = LogManager.getLogger(ElementActions.class);
     public ElementActions(GUIDriver driver){
         this.driver = driver;
         waits = new Waits(driver);
         scrolling = new Scrolling(driver);
     }
-    // 1. ميثود الـ Allure الخاصة بالصور (دي اللي بتعرض الصور في التقرير)
     @Attachment(value = "{0}", type = "image/png")
     public static byte[] saveScreenshotToAllure(String name, WebDriver driver) {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
@@ -40,10 +38,8 @@ public class ElementActions {
             WebElement element = findElement(locator);
             element.clear();
             element.sendKeys(key);
-            // إضافة Log عند النجاح
             LogsUtil.info("data entered: ",key, "in the field: ", locator.toString());
         } catch (Exception e) {
-            // إضافة Log عند الفشل
             LogsUtil.error("Failed to type " ,key," into element: ",locator.toString() );
             throw e;
         }
@@ -55,10 +51,8 @@ public class ElementActions {
             scrolling.ScrollToElement(locator);
 
             try {
-                // محاولة الضغط العادي
                 findElement(locator).click();
             } catch (Exception e) {
-                // لو فشل (زي حالة Firefox headless)، نستخدم JS كـ Backup
                 LogsUtil.warn("Normal click failed, trying JavaScript click for: " + locator);
                 ((JavascriptExecutor) driver.getDriver()).executeScript("arguments[0].click();", findElement(locator));
             }
@@ -83,24 +77,17 @@ public class ElementActions {
     }
     public static String takeScreenShot(WebDriver driver, String screenshotName){
         try {
-            // إنشاء الفولدر لو مش موجود
             File folder = new File(System.getProperty("user.dir") + "/screenshots");
             if (!folder.exists()) {
                 folder.mkdirs();
             }
-            // 1. تحويل الـ Driver لـ TakeScreenshot
             TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-            // 2. التقاط الصورة كملف
             File source =takesScreenshot.getScreenshotAs(OutputType.FILE);
-            // 3. تحديد المسار (فولدر اسمه screenshots)
             String destination = System.getProperty("user.dir") + "/screenshots/" + screenshotName + "_" + System.currentTimeMillis() + ".png";
 
              File finalDestination = new File(destination);
-            // 4. حفظ الملف
             FileHandler.copy(source, finalDestination);
-            // نداء ميثود الـ Allure عشان تبعت الصورة للتقرير برضه
             saveScreenshotToAllure(screenshotName, driver);
-            // لوج للتأكيد إن الصورة اتسجلت
             LogsUtil.info("Screenshot saved at: " + destination);
             return destination;
         } catch (IOException e){
