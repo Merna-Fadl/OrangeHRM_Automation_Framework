8e26e1e8e1ad932056f479f3ad5a9c5a2cbe370c
Merna-Fadl <137529881+Merna-Fadl@users.noreply.github.com>
2026-05-22 19:08:36 +0300

    Update LoginPage.java

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -11,23 +11,22 @@ public class LoginPage {
     public LoginPage(GUIDriver driver){
         this.driver = driver;
     }
-    // 1. Locators (مواقع العناصر في صفحة OrangeHRM)
+    // 1. Locators 
     private By usernameField = By.name("username");
     private By passwordField = By.name("password");
     private By loginButton = By.xpath("//button[@type='submit']");
 
-    // العنصر ده بيظهر بس لما اللوجين ينجح (اسم المستخدم فوق عاليمين)
- //   private By userDropdown = By.className("oxd-userdropdown-name");
+    
     private By userDropdown = By.xpath("//p[@class='oxd-userdropdown-name']");
-    // لوكيتور رسالة الخطأ (Invalid credentials)
+    // (Invalid credentials)
     private By errorMessage = By.xpath("//div[@role='alert']//p");
-    // لوكيتور رسالة الـ Required (بتظهر تحت الفيلد نفسه)
+    
     private By inputValidationError = By.xpath("//span[contains(@class,'oxd-input-group__message')]");
 
-    // 2. Methods (Actions) - Fluent Design (بتعمل return this عشان نربط الخطوات)
+    // 2. Methods (Actions) - Fluent Design 
     @Step("Entering username: {user}")
     public LoginPage enterUsername(String user) {
-        // بننادي ميثود الـ sendKey من الـ ElementActions اللي عندك
+        
         driver.elementActions().sendKey(usernameField,user);
         return this;
     }
@@ -40,25 +39,22 @@ public class LoginPage {
 
     @Step("Clicking Login button")
     public LoginPage clickLogin() {
-        // بنستخدم ميثود الـ clickElement من كلاس الـ Utils بتاعك
+        
         driver.elementActions().clickElement(loginButton);
         return this;
     }
-    // 3. Validation (التحقق من نجاح العملية)
+    // 3. Validation 
 
     @Step("Verifying successful login by URL and Header")
     public void assertSuccessfulLogin() {
-        // استخدمي الـ waits اللي عندك في الـ ElementActions
+        
         driver.waits().waitForElementVisible(userDropdown);
-        // التحقق الأول: إن الرابط الجديد فيه كلمة dashboard (لأن ده ERP حقيقي)
         String actualUrl = driver.getDriver().getCurrentUrl();
         Assert.assertTrue(actualUrl.contains("dashboard"), "Login failed - URL mismatched! Actual: " + actualUrl);
 
-        // التحقق الثاني: إن الـ Dropdown بتاع اليوزر ظهر (معناه إننا جوه السيستم)
         boolean isUserMenuDisplayed = driver.getDriver().findElement(userDropdown).isDisplayed();
         Assert.assertTrue(isUserMenuDisplayed, "Login failed - User dropdown not displayed!");
 
-        // سحب النص الحقيقي من الـ Dropdown للتحقق منه
         String actualUserName = driver.getDriver().findElement(userDropdown).getText();
         Assert.assertFalse(actualUserName.isEmpty(), "User name is empty! Login might have failed.");
     }
